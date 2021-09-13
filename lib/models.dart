e3d1eea9033dae2cdf1e1d9819d0c71b9d13ce87
Timothy Ogbemudia <ogbemudiatimothy@gmail.com>
2021-09-13 11:06:47 +0100

    feat: created data fetching models

---
+++ lib/models.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+// function for converting from JSON to an object (or map)
+
+PhoneCheck phoneCheckFromJSON(String jsonString) =>
+    PhoneCheck.fromJson(json.decode(jsonString));
+
+class PhoneCheck {
+  String checkId;
+  String checkUrl;
+
+  PhoneCheck({required this.checkId, required this.checkUrl});
+
+  factory PhoneCheck.fromJson(Map<String, dynamic> json) => PhoneCheck(
+        checkId: json["check_id"],
+        checkUrl: json["check_url"],
+      );
+}
